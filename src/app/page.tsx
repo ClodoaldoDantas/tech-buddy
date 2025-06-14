530e34f0c652baa8c21093561929d1e2b2431fc9
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-06-14 15:24:43 -0300

    feat: integrate authentication with GitHub using NextAuth and Prisma
    - Added Prisma schema for user accounts, sessions, and verification tokens.
    - Implemented authentication logic in `src/lib/auth.ts` using NextAuth with GitHub provider.
    - Created a Prisma client instance in `src/lib/prisma.ts` for database interactions.
    - Updated layout and home page to reflect new app title and description.
    - Added sign-in button on the home page that triggers GitHub authentication.
    - Introduced middleware for authentication handling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import { Button } from '@/components/ui/button'
+import { signIn } from '@/lib/auth'
 
 export default function Home() {
-  return <Button>Sign In</Button>
+  return (
+    <div className="flex h-dvh w-full items-center justify-center">
+      <form
+        action={async () => {
+          'use server'
+          await signIn('github')
+        }}
+      >
+        <Button type="submit" size="lg">
+          Entrar com Github
+        </Button>
+      </form>
+    </div>
+  )
 }
